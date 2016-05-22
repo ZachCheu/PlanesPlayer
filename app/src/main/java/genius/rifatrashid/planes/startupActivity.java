f3de747fd2e44d8375dd1a5755fc0529f69f7ffa
rifatrashid <s-RRashid@lwsd.org>
2016-05-22 08:41:19 -0700

    dsf

--- app/src/main/java/genius/rifatrashid/planes/startupActivity.java
+++ app/src/main/java/genius/rifatrashid/planes/startupActivity.java
@@ -1,7 +1,38 @@
 package genius.rifatrashid.planes;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PowerManager;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 /**
  * Created by rifatrashid on 5/22/16.
  */
-public class startupActivity {
+public class startupActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewpager_activity);
+
+
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 }
